f0f54fa8af693b0c21133b8678c69c51145be4b1
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-27 19:35:13 -0400

    update

---
+++ blog/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ blog/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+      </body>
+    </Html>
+  )
+}
